03bdab82ccceb2e19bc9237c8d12699070fae1e2
Saulius <sauliuxx@gmail.com>
2019-02-06 11:00:00 +0200

    Update on task class to build.

--- src/main/java/com/example/demo/task/Task.java
+++ src/main/java/com/example/demo/task/Task.java
@@ -1,16 +1,24 @@
 package com.example.demo.task;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * The Class Task.
  */
 @Entity
+@NoArgsConstructor
 @Table(name = "task")
 @Data
 public class Task {
 
+	@Id
+	@GeneratedValue
+	private long id;
+	
 }
